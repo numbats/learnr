689660e2af6c44dc8c67ea5a05308d4e7472b8dd
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-11-24 00:31:51 +1100

    Specify deploy branch for action

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const autoprefixer = require('autoprefixer')
 
 module.exports = {
     siteMetadata: meta,
+    pathPrefix: "/learnr",
     plugins: [
         {
             resolve: `gatsby-plugin-sass`,
