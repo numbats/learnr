5290251c42ce32f170683388cd9c770d80798a62
Emi Tanaka <emi.tanaka@monash.edu>
2021-11-29 22:33:28 +1100

    update exercise to a more sensible one. No qplot!

--- exercises/solution_01_03.R
+++ exercises/solution_01_03.R
@@ -1,17 +1,14 @@
-library(ggplot2)
+library(tidyverse)
 
-mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
-  	labels=c("3gears","4gears","5gears"))
-mtcars$am <- factor(mtcars$am,levels=c(0,1),
-  	labels=c("Automatic","Manual"))
-mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
-   labels=c("4cyl","6cyl","8cyl"))
+mtcars <- mtcars %>%
+  mutate(am = factor(am, levels = 0:1, labels = c("Automatic", "Manual")))
+
 
 # Print the gear variable of mtcars
-print(mtcars$gear)
+mtcars$gear
 
-# Assign the length of mtcars to some_var
-some_var <- length(mtcars)
+# Assign the number of rows in mtcars to `n`
+n <- nrow(mtcars)
 
 # Uncomment this to see the plot
-# print(qplot(mpg, data=mtcars, geom="density", fill=gear, alpha=I(.5)))
+# ggplot(mtcars, aes(am, mpg)) + geom_violin()
--- exercises/test_01_03.R
+++ exercises/test_01_03.R
@@ -2,10 +2,10 @@ test <- function() {
     # Here we can either check objects created in the solution code, or the
     # string value of the solution, available as .solution. See the testTemplate
     # in the meta.json for details.
-    if (some_var != length(mtcars)) {
+    if (n != nrow(mtcars)) {
         stop("Are you getting the correct length?")
     }
-    if (!grepl("print(mtcars$gear)", .solution, fixed = TRUE)) {
+    if (!grepl("mtcars$gear", .solution, fixed = TRUE)) {
         stop("Are you printing the correct variable?")
     }
 
