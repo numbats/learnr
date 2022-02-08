68a0b5df5016518d62c86b3f960195bbe3328b54
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-02-09 00:30:06 +1100

    Add introduction to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,19 @@ export default ({ data }) => {
     return (
         <Layout isHome>
             <Logo className={classes.logo} aria-label={siteMetadata.title} />
+            <section>
+                <h1 className={classes.subtitle}>An interactive introduction to data analysis with R</h1>
+                <div className={classes.introduction}>
+                <p>
+                    Hello! Let's learn <svg viewBox="0 0 581 512" xmlns="http://www.w3.org/2000/svg" style={{height: '1em'}}>
+  <path d="M581 226.6C581 119.1 450.9 32 290.5 32S0 119.1 0 226.6C0 322.4 103.3 402 239.4 418.1V480h99.1v-61.5c24.3-2.7 47.6-7.4 69.4-13.9L448 480h112l-67.4-113.7c54.5-35.4 88.4-84.9 88.4-139.7zm-466.8 14.5c0-73.5 98.9-133 220.8-133s211.9 40.7 211.9 133c0 50.1-26.5 85-70.3 106.4-2.4-1.6-4.7-2.9-6.4-3.7-10.2-5.2-27.8-10.5-27.8-10.5s86.6-6.4 86.6-92.7-90.6-87.9-90.6-87.9h-199V361c-74.1-21.5-125.2-67.1-125.2-119.9zm225.1 38.3v-55.6c57.8 0 87.8-6.8 87.8 27.3 0 36.5-38.2 28.3-87.8 28.3zm-.9 72.5H365c10.8 0 18.9 11.7 24 19.2-16.1 1.9-33 2.8-50.6 2.9v-22.1z"></path>
+</svg>!
+                </p>
+                <p>
+                    In this course, you'll learn the basics of using R for data analysis. This should provide you with the necessary skills to use R when learning more advanced and specialised topics. You don't need any prior experience with R, statistics, or programming to work through this material, however if you already have some experience you can start from any chapter you'd like to learn from.
+                </p>
+                </div>
+            </section>
             {chapters.map(({ slug, title, description }) => (
                 <section key={slug} className={classes.chapter}>
                     <h2 className={classes.chapterTitle}>
