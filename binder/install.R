097f3e49a38c656a2bb08e8557683ee2fabf4a61
Mitchell O'Hara-Wild <mitchelloharawild@users.noreply.github.com>
2022-02-09 12:48:20 +1100

    Update R startup

--- binder/install.R
+++ binder/install.R
@@ -1,2 +1,8 @@
 install.packages("tidyverse")
 install.packages("rjson")
+install.packages("Ecdat")
+install.packages("testthat")
+install.packages("patchwork")
+install.packages("broom")
+
+options(tidyverse.quiet = TRUE)
