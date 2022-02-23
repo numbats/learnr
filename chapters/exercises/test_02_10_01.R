98ced5acfc44733e666079053078fc41b013ad26
Jessica Leung <54132201+jessica-wyleung@users.noreply.github.com>
2022-02-23 15:38:57 +1100

    fixing test

--- chapters/exercises/test_02_10_01.R
+++ chapters/exercises/test_02_10_01.R
@@ -1,7 +1,7 @@
 library(testthat)
   
 test <- function() {
-  if my_result[5] != 0.00198{
+  if (my_result[5] != 0.00198){
     stop("Try again.")
   }
   success("Well done!")
--- chapters/exercises/test_02_11_01.R
+++ chapters/exercises/test_02_11_01.R
@@ -1,7 +1,7 @@
 library(testthat)
 
 test <- function() {
-    if power_three(20) != 20^3{
+    if (power_three(20) != 20^3){
       stop("Did you correctly define the function power_three()? Check the function body. Are you taking the input argument to the power three?")
     }
     success("Well done!")
